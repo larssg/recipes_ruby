3873899e70ee79ba0a455a62ffe25652e39072a5
Lars G. Sehested <lgs@lenio.dk>
2009-02-21 15:51:52 +0100

    Added metric_fu

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "thoughtbot-clearance", :lib => 'clearance', :source  => 'http://gems.github.com'
+  config.gem 'jscruggs-metric_fu', :version => '0.9.0', :lib => 'metric_fu', :source => 'http://gems.github.com'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
